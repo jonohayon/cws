73fc52cced25977f19ad654ab7d7f1c36a936e6d
Jonathan Ohayon <jonathanohayon0@gmail.com>
2021-11-13 20:13:45 +0200

    Added string list utility code

---
+++ cws-utils/utils.c
@@ -0,0 +1,114 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include "cws-utils/utils.h"
+
+#define ASSERT_NON_EMPTY_STRING_LIST(_string_list) do { \
+    ASSERT_NON_NULL((_string_list)); \
+    ASSERT_NON_NULL((_string_list)->strings); \
+    ASSERT(0 != (_string_list)->length, ZERO_SIZED_LIST); \
+    ASSERT(0 != (_string_list)->max_size, ZERO_SIZED_LIST); \
+  } while (0)
+
+const char COMMA[] = ",";
+
+/**
+ * Removes the whitespace from the beginning and end of the given string. Done in-place.
+ *
+ * @param [in,out] str The string to trim
+ * @param [out] output_length The length of the trimmed string
+ * @returns The status of the trim operation
+ */
+static cws_status_code_t trim_string(char *input, size_t *output_length) {
+  cws_status_code_t retval = SUCCESS;
+
+  ASSERT_NON_NULL(input);
+  ASSERT_NON_NULL(output_length);
+
+  size_t string_length = strlen(input);
+
+  // Remove the spaces from the length count
+  for (; isspace(input[string_length - 1]); string_length--);
+
+  *output_length = string_length;
+
+  // Eat up the whitespace from the start of the string
+  for (; isspace(*input); input++);
+
+cleanup:
+  return retval;
+}
+
+cws_status_code_t create_string_list(cws_string_list_t *string_list) {
+  cws_status_code_t retval = SUCCESS;
+
+  ASSERT_NON_NULL(strings_list);
+
+  ASSERT(0 != strings_list->length, ZERO_SIZED_LIST);
+
+  strings_list->strings = (cws_string_t *) calloc(strings_list->length, sizeof(cws_string_t));
+  strings_list->max_size = strings_list->length;
+
+cleanup:
+  return retval;
+}
+
+cws_status_code_t add_string_to_list(cws_string_list_t *string_list, char *in_string,
+    size_t string_length) {
+  cws_status_code_t retval = SUCCESS;
+  cws_string_t *new_string = NULL;
+
+  ASSERT_NON_EMPTY_STRING_LIST(string_list);
+
+  // Resize the list if it's already full
+  if (string_list->length == string_list->max_size) {
+    string_list->max_size *= 2;
+    string_list->strings = (cws_string_t *) realloc(string_list->max_size * sizeof(cws_string_t));
+
+    ASSERT_NON_EMPTY_STRING_LIST(string_list);
+  }
+
+  new_string = string_list->strings[string_list->length];
+  new_string->data = in_string;
+  new_string->length = string_length;
+  string_list->length++;
+
+cleanup:
+  return retval;
+}
+
+cws_status_code_t release_string_list(cws_string_list_t *string_list) {
+  cws_status_code_t retval = SUCCESS;
+
+  ASSERT_NON_EMPTY_STRING_LIST(string_list);
+
+  free(strings_list->strings);
+
+cleanup:
+  return retval;
+}
+
+cws_status_code_t parse_comma_separated_list(char *in_values, cws_string_list_t *out_list) {
+  cws_status_code_t retval = SUCCESS;
+
+  ASSERT_NON_NULL(in_values);
+  ASSERT_NON_NULL(out_values);
+  ASSERT_NON_NULL(values_length);
+
+  *values_length = 1;
+  *out_values = (cws_string_t *) calloc(*values_length, sizeof(cws_string_t));
+
+  size_t trimmed_size = 0;
+  char *current_value = strtok(in_values, COMMA);
+  while (NULL != current_value) {
+    retval = trim_string(current_value, &trimmed_size);
+    ASSERT_SUCCESS();
+
+    retval = add_string_to_list(out_list, current_value, trimmed_size);
+    ASSERT_SUCCESS();
+  }
+
+cleanup:
+  return retval;
+}
--- cws-utils/utils.h
+++ cws-utils/utils.h
@@ -1,11 +1,50 @@
 #include <stdint.h>
+#include <stdlib.h>
 
 #ifndef CWS_UTILS__UTILS_H
 #define CWS_UTILS__UTILS_H
 
+#define ASSERT(_cond, _retval) do { \
+    if (!(_cond)) { \
+      retval = (_retval); \
+      goto cleanup; \
+    } \
+  } while (0)
+
+#define ASSERT_NON_NULL(_variable) ASSERT(NULL != (_variable), NULL_POINTER_GIVEN)
+
+#define ASSERT_SUCCESS() ASSERT(SUCCESS == retval, retval)
+
+typedef enum cws_status_code_e {
+  SUCCESS = 0,
+  NULL_POINTER_GIVEN,
+  ZERO_SIZED_LIST,
+} cws_status_code_t;
+
 typedef struct cws_string_s {
   char *data;
-  uint32_t length;
+  size_t length;
+  size_t max_size;
 } cws_string_t;
+
+typedef struct cws_string_list_s {
+  size_t length;
+  cws_string_t strings[0];
+} cws_string_list_t;
+
+cws_status_code_t create_string_list(cws_string_list_t *string_list);
+
+cws_status_code_t add_string_to_list(cws_string_list_t *string_list, char *in_string,
+    size_t string_length);
+
+cws_status_code_t release_string_list(cws_string_list_t *string_list);
+
+/**
+ * Parses a given comma-separated list of values into an array of strings.
+ * 
+ * @param [in] in_values The input comma-separated list of values
+ * @param [out] out_list The output list of values from the input list
+ */
+cws_status_code_t parse_comma_separated_list(char *in_values, cws_string_list_t *out_list);
 
 #endif // CWS_UTILS__UTILS_H
